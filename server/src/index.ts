7a58f73c4bfcb6a4fb333c7b19ee385fc99700f1
Егор Пантелеев <kakeparake@gmail.com>
2023-12-24 03:41:17 +0300

    feat(index): images uploading

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
+import multer from "multer";
+import fs from "fs";
+
 import {
   loginValidation,
   postCreateValidation,
@@ -10,6 +13,7 @@ import * as UserController from "./controllers/UserController";
 import * as PostController from "./controllers/PostController";
 
 import checkAuth from "./utils/checkAuth";
+
 mongoose
   .connect(
     "mongodb+srv://admin:admin@mern-posts-db.rvisudm.mongodb.net/mern-posts?retryWrites=true&w=majority"
@@ -24,11 +28,34 @@ mongoose
 const port = 3001;
 const app = express();
 
+const uploadsDir = "./uploads";
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, uploadsDir);
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
 app.post("/auth/login", loginValidation, UserController.login);
 app.post("/auth/register", registerValidation, UserController.register);
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `uploads/${req.file?.originalname}`,
+  });
+});
 
 app.get("/posts", PostController.getAll);
 app.get("/posts/:id", PostController.getOne);
