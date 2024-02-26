ac0f0abf053d6b25415a61a75680d034a0be9e9d
Егор Пантелеев <kakeparake@gmail.com>
2024-02-26 09:42:28 +0300

    fix(modal): incorrect display

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -19,29 +19,30 @@ export function Navbar() {
   };
 
   return (
-    <nav className="w-full flex fixed z-[10] bg-white justify-center pt-[10px]">
-      <div className="w-[90%] p-[10px] rounded-[6px] flex justify-between items-center border-[1px] shadow-lg">
-        <Link to="/">
-          <span className="font-[700] transition duration-200 hover:text-blue-900">
-            MERN-POSTS
-          </span>
-        </Link>
-        <div className="flex flex-row gap-[5px] font-[700] text-[13px]">
-          <button
-            onClick={() => setShowLogin(true)}
-            className="bg-blue-100 px-[10px] py-[5px] rounded-[6px] hover:scale-[1.05] hover:bg-blue-200 transition duration-200"
-          >
-            LOGIN
-          </button>
-          <button
-            onClick={() => setShowRegister(true)}
-            className="bg-green-100 px-[10px] py-[5px] rounded-[6px] hover:scale-[1.05] hover:bg-green-200 transition duration-200"
-          >
-            REGISTER
-          </button>
+    <>
+      <nav className="w-full flex fixed z-[10] bg-white justify-center pt-[10px]">
+        <div className="w-[90%] p-[10px] rounded-[6px] flex justify-between items-center border-[1px] shadow-lg">
+          <Link to="/">
+            <span className="font-[700] transition duration-200 hover:text-blue-900">
+              MERN-POSTS
+            </span>
+          </Link>
+          <div className="flex flex-row gap-[5px] font-[700] text-[13px]">
+            <button
+              onClick={() => setShowLogin(true)}
+              className="bg-blue-100 px-[10px] py-[5px] rounded-[6px] hover:scale-[1.05] hover:bg-blue-200 transition duration-200"
+            >
+              LOGIN
+            </button>
+            <button
+              onClick={() => setShowRegister(true)}
+              className="bg-green-100 px-[10px] py-[5px] rounded-[6px] hover:scale-[1.05] hover:bg-green-200 transition duration-200"
+            >
+              REGISTER
+            </button>
+          </div>
         </div>
-      </div>
-
+      </nav>
       {showLogin && (
         <Modalbox
           width={"300px"}
@@ -77,7 +78,6 @@ export function Navbar() {
           </form>
         </Modalbox>
       )}
-
       {showRegister && (
         <Modalbox
           width={"300px"}
@@ -128,6 +128,6 @@ export function Navbar() {
           </form>
         </Modalbox>
       )}
-    </nav>
+    </>
   );
 }
--- client/src/components/common/Modalbox.tsx
+++ client/src/components/common/Modalbox.tsx
@@ -1,6 +1,5 @@
 import { mdiClose } from "@mdi/js";
 import Icon from "@mdi/react";
-import { createPortal } from "react-dom";
 
 interface ModalType {
   onClose: () => void;
@@ -11,38 +10,33 @@ interface ModalType {
 
 export function Modalbox(props: ModalType) {
   return (
-    <>
-      {createPortal(
-        <div
-          onClick={() => props.onClose()}
-          className="absolute w-full h-full inset-0 z-[20] flex justify-center items-center backdrop-blur-sm bg-black/15"
-        >
-          <div
-            onClick={(event) => event.stopPropagation()}
-            style={{ width: props.width }}
-            className="bg-white p-[20px] border shadow-lg rounded-[6px]"
+    <div
+      onClick={() => props.onClose()}
+      className="absolute w-full h-full inset-0 z-[20] flex justify-center items-center backdrop-blur-sm bg-black/15"
+    >
+      <div
+        onClick={(event) => event.stopPropagation()}
+        style={{ width: props.width }}
+        className="bg-white p-[20px] border shadow-lg rounded-[6px]"
+      >
+        <div className="flex flex-row mb-[10px] justify-between">
+          <p className="font-[700]">{props.title}</p>
+          <button
+            className="hover:scale-[1.3] transition duration-200"
+            onClick={() => props.onClose()}
           >
-            <div className="flex flex-row mb-[10px] justify-between">
-              <p className="font-[700]">{props.title}</p>
-              <button
-                className="hover:scale-[1.3] transition duration-200"
-                onClick={() => props.onClose()}
-              >
-                <Icon
-                  path={mdiClose}
-                  size={0.8}
-                  horizontal
-                  vertical
-                  rotate={90}
-                  color="black"
-                />
-              </button>
-            </div>
-            {props.children}
-          </div>
-        </div>,
-        document.body
-      )}
-    </>
+            <Icon
+              path={mdiClose}
+              size={0.8}
+              horizontal
+              vertical
+              rotate={90}
+              color="black"
+            />
+          </button>
+        </div>
+        {props.children}
+      </div>
+    </div>
   );
 }
